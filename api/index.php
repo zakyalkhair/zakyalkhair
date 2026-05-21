0b946f155d248a8b892da8ad67433e8545d8a1e1
Zaky <muhzakykhair@gmail.com>
2026-05-22 06:41:35 +0700

    Add Laravel bootstrap cache for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/views';
+
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
 require __DIR__ . '/../public/index.php';
